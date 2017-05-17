e777ea10cb4b28f5b811a2f7c81d46f02eba1dd9
rokasveitas <rokas.veitas@gmail.com>
2017-05-17 10:50:22 -0700

    SpingThing and Paddle creation

--- SpingPongProject/Paddle.java
+++ SpingPongProject/Paddle.java
@@ -4,6 +4,17 @@ public class Paddle extends SpingThing
     public double omg; //omega; angular velocity
     public double alp; //alpha; angular acceleration
     
+    public Paddle(double width, double ang, Vector pos)
+    {
+        super(width);
+        this.pos = pos;
+        this.vel = new Vector(0, 0);
+        this.acc = new Vector(0, 0);
+        this.ang = ang;
+        this.omg = 0;
+        this.alp = 0;
+    }
+    
     public void timeInc(double dt) //changes vel and pos as time changes by dt
     {
         super.timeInc(dt);
