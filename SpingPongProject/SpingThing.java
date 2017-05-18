1d6e9a47b3e102487fce9c8a9ef08e835235604e
rokasveitas <rokas.veitas@gmail.com>
2017-05-18 10:12:05 -0700

    Adding back GitHub stuff?

--- SpingPongProject/SpingThing.java
+++ SpingPongProject/SpingThing.java
@@ -3,15 +3,7 @@ public class SpingThing
     public Vector acc; //acceleration
     public Vector vel; //velocity
     public Vector pos; //position
-    public double size; //width if paddle, radius if ball
-    
-    public SpingThing(double size)
-    {
-        acc = new Vector(0, 0);
-        vel = new Vector(0, 0);
-        pos = new Vector(0, 0);
-        this.size = size;
-    }
+    public double spin; //spin; positive is clockwise
     
     public void timeInc(double dt) //changes vel and pos as time changes by dt
     {
--- SpingPongProject/Vector.java
+++ SpingPongProject/Vector.java
@@ -2,62 +2,69 @@ import java.lang.Math;
 
 public class Vector
 {
-	double x, y, r, theta;
-	public Vector(double x, double y)
-	{
-		this.x = x;
-		this.y = y;
-		this.r = Math.sqrt(x*x + y*y);
-		this.theta = Math.atan(y / x);
-	}
-	public double getR()
-	{
-		return r;
-	}
-	public double getTheta()
-	{
-		return theta;
-	}
-	public double getX()
-	{
-		return x;
-	}
-	public double getY()
-	{
-		return y;
-	}
-	public void setX(double x)
-	{
-		this.x = x;
-		this.r = Math.sqrt(x*x + y*y);
-		this.theta = Math.atan(y / x);
-	}
-	public void setY(double y)
-	{
-		this.y = y;
-		this.r = Math.sqrt(x*x + y*y);
-		this.theta = Math.atan(y / x);
-	}
-	public void rotate(double theta)
-	{
-		this.theta += theta;
-		x = r * Math.cos(this.theta);
-		y = r * Math.sin(this.theta);
-	}	
-	public static Vector add(Vector a, Vector b)
-	{
-		double newX = a.getX() + b.getX();
-		double newY = a.getY() + b.getY();
-		return new Vector(newX, newY);
-	}
-	public static double dot(Vector a, Vector b)
-	{
-		return a.getX() * b.getX() + a.getY() * b.getY();
-	}
-	public static Vector unit(double theta)
-	{
-		double newX = Math.cos(theta);
-		double newY = Math.sin(theta);
-		return new Vector(newX, newY);
-	}
+    double x, y, r, theta;
+    public Vector(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+        this.r = Math.sqrt(x*x + y*y);
+        this.theta = Math.atan(y / x);
+        /*if(x < 0)
+        {
+            if(y >= 0) //quad II
+                theta += Math.PI / 2;
+            else
+                theta -= Math.PI / 2;
+        } */               
+    }
+    public double getR()
+    {
+        return r;
+    }
+    public double getTheta()
+    {
+        return theta;
+    }
+    public double getX()
+    {
+        return x;
+    }
+    public double getY()
+    {
+        return y;
+    }
+    public void setX(double x)
+    {
+        this.x = x;
+        this.r = Math.sqrt(x*x + y*y);
+        this.theta = Math.atan(y / x);
+    }
+    public void setY(double y)
+    {
+        this.y = y;
+        this.r = Math.sqrt(x*x + y*y);
+        this.theta = Math.atan(y / x);
+    }
+    public void rotate(double theta)
+    {
+        this.theta += theta;
+        x = r * Math.cos(this.theta);
+        y = r * Math.sin(this.theta);
+    }   
+    public static Vector add(Vector a, Vector b)
+    {
+        double newX = a.getX() + b.getX();
+        double newY = a.getY() + b.getY();
+        return new Vector(newX, newY);
+    }
+    public static double dot(Vector a, Vector b)
+    {
+        return a.getX() * b.getX() + a.getY() * b.getY();
+    }
+    public static Vector unit(double theta)
+    {
+        double newX = Math.cos(theta);
+        double newY = Math.sin(theta);
+        return new Vector(newX, newY);
+    }
 }
