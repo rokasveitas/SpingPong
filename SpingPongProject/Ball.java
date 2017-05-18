7c521545f29d7b3730afb49a3cdda0056127d7be
rokasveitas <rokas.veitas@gmail.com>
2017-05-18 11:20:28 -0700

    Collide method
    It's been started!

--- SpingPongProject/Ball.java
+++ SpingPongProject/Ball.java
@@ -1,5 +1,7 @@
 public class Ball extends SpingThing
 {
+    public static final double SPIN_K = 1;
+    public static final double SPIN_DEC = .8;
     public double spin; //positive is clockwise
     
     public Ball(double rad, Vector pos, Vector vel)
@@ -8,5 +10,19 @@ public class Ball extends SpingThing
         this.pos = pos;
         this.vel = vel;
         this.acc = new Vector(0, 0);
+    }
+    
+    public void collide(SpingThing col) throws Exception
+    {
+        switch(col.type)
+        {
+            case "table":
+                this.vel.setY(-1 * this.vel.getY());
+                this.vel.setX(this.vel.getX() + SPIN_K * this.spin);
+                spin *= SPIN_DEC;
+                break;
+            default:
+                throw new Exception("SpingThing col needs type.");
+        }
     }
 }
